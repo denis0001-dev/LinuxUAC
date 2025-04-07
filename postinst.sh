daa99adf6b4cd3b0bddeca5c1d41680dfce3b003
denis0001-dev <demorozov2048@gmail.com>
2025-04-07 13:01:49 +0300

    small fix

--- postinst.sh
+++ postinst.sh
@@ -8,7 +8,7 @@ cat << "FEOF" > "$pkcommand"
 #!/bin/bash
 cat << SEOF > /tmp/uac_askpass.sh
 \#!/bin/bash
-command=$(echo "$@" | sed -E "s/^$/\/bin\/bash/gm;t")
+command=$(echo "$@" | sed -E 's/^$/\/bin\/bash/gm;t')
 uac "--app=\$command"
 SEOF
 chmod +x /tmp/uac_askpass.sh
