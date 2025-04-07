f34340068adb232cd22ef30f606078e2a9f934d8
denis0001-dev <demorozov2048@gmail.com>
2025-04-07 13:09:38 +0300

    small fix

--- postinst.sh
+++ postinst.sh
@@ -6,11 +6,11 @@ mv "$pkcommand" "$pkcommand.bak"
 
 cat << "FEOF" > "$pkcommand"
 #!/bin/bash
-cat << SEOF > /tmp/uac_askpass.sh
-\#!/bin/bash
-command=$(echo "$@" | sed -E 's/^$/\/bin\/bash/gm;t')
-uac "--app=\$command"
-SEOF
+
+echo "#!/bin/bash" > /tmp/uac_askpass.sh
+echo "command=$(echo "$@" | sed -E 's/^$/\/bin\/bash/gm;t')" >> /tmp/uac_askpass.sh
+echo "uac \"--app=\$command\"" >> /tmp/uac_askpass.sh
+
 chmod +x /tmp/uac_askpass.sh
 SUDO_ASKPASS=/tmp/uac_askpass.sh sudo -A $@
 code=$?
